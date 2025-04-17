e4302bb3f306fa5aab827e585425e35c72402cce
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-04-17 12:15:20 -0600

    started figuring out using node

--- sketch.js
+++ sketch.js
@@ -1,24 +1,11 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-import changeFileExtension from 'change-file-extension';
 
-const decompress = require('decompress');
-
-changeFileExtension("mapFile.osk", ".zip");
-
-decompress("mapFile.zip", "maps").then(files => {
-  console.log("done");
-});
-
+import decompress from "decompress";
 
 let gameState = "mainMenu";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);  
+  decompress("mapFile.zip", "maps");
 }
 
 function draw() {
