111b805a16391ae047a0a626a7cc62fced12cb55
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-04-29 14:14:48 -0600

    added hit circles and maps

--- proposal.md
+++ proposal.md
@@ -5,12 +5,13 @@ clone of a rhythm game osu!
 it is a rhythm game where you click circles to the beat of a song
 
 ### Needs To Have List
-- hit circles slider and spinners
+- hit circles slider
 - song select
 - mods (like dt, hd, ez, hr)
     - dt double time speeds up the gameplay by x1.5
     - hd hidden shows a flash of an object then hides it
     - ez increases object size and lowers approch rate
+    - hr flips playfield verticaly increaes circle size and approach rate
     
 
 ### Nice To Have List
--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
-const zl = require("zip-lib");
+//https://osu.ppy.sh/wiki/en/Client/File_formats/osu_%28file_format%29
+//https://osu.ppy.sh/wiki/en/Beatmap/Circle_size
+//https://osu.ppy.sh/wiki/en/Beatmap/Approach_rate
 
-zl.extract("/mapFile.zip", "/maps/");
+let osuScaleY;
+let osuScaleX;
+
+function preload() {
+  hitCircleImage = loadImage("/hitcircles/hitcircle.png");
+  approachCircleImage = loadImage("/hitcircles/approachcircle.png");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);  
+  createCanvas(1024, 768);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
+  image(hitCircleImage, width/2, height/2);
+  rect(0, 0, );
 }
