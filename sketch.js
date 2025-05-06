96e35ef66aa8e99b904c36bafec0f6083fc97682
BlueToffe <63087459+BlueToffe@users.noreply.github.com>
2025-05-06 14:15:45 -0600

    started parsing through hit circle location and hit circle data

--- sketch.js
+++ sketch.js
@@ -2,23 +2,28 @@
 //https://osu.ppy.sh/wiki/en/Beatmap/Circle_size
 //https://osu.ppy.sh/wiki/en/Beatmap/Approach_rate
 
-let hitCircleInfo;
+
+
 let apporachRate;
 let mapDifficuty;
-class MapData {
+let comboColours =  {
+  Combo1: [184, 213, 255],
+  Combo2: [231, 189, 255],
+  Combo3: [199, 240, 255],
+  Combo4: [255, 194, 255],
+};
+
+class hitCircleInfo {
   constructor() {
-    this.apporachRate;
-    this.circleSize;
-    this.overallDifficulty;
-    this.hpDrain;
-    this.mapArray;
+    this.objectString;
   }
-
-
+  // use split to section off the array into its part the from there wer will be assiging a pointer to object x and y
+  
 }
 
 function preload() {
   hitCircleImage = loadImage("/hitcircles/hitcircle.png");
+  hitCircleOverlay = loadImage("/hitcircles/hitcircleoverlay.png");
   approachCircleImage = loadImage("/hitcircles/approachcircle.png");
   loadStrings("/maps/GenryuuKaiko/higantorrent.osu", loadMap); 
 }
@@ -31,12 +36,18 @@ function setup() {
 function draw() {
   background(220);
   image(hitCircleImage, width/2, height/2);
+  image(hitCircleOverlay, width/2, height/2);
 }
 
 function loadMap(data) {
   for (let mapStats = 0; mapStats < data.length; mapStats++) {
+    let previousSection = null;
     if (data[mapStats] === "[Difficulty]") {
       mapDifficuty = data.splice(mapStats + 1, 6);
+    }
+
+    if (data[mapStats] === "[HitObjects]") {
+      hitCircles = data.splice(mapStats + 1, data.length - mapStats);
     }
   }
 }
